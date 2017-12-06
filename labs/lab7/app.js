42887a804e5bd44b042967e0123db4ba7e8a4e2f
Anna Rodrigez <9rails@gmail.com>
2017-12-07 00:58:16 +0200

    since my laptop is dead i'm starting everuthing again from scretch

---
+++ labs/lab7/app.js
@@ -0,0 +1,256 @@
+const events = require('./modules/events.js');
+const users = require('./modules/user.js');
+const construcor = require('./modules/event.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const crypto = require('crypto');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const multer = require('multer');
+const app = express();
+let config = require('./config');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/jq', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let upload = multer({dest: 'public/images/' });
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+    secret: 'SEGReT$25_',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const serverSalt = "45%sAlT_";
+
+function sha512(password, salt) {
+    const hash = crypto.createHmac('sha512', salt);
+    hash.update(password);
+    const value = hash.digest('hex');
+    return {
+        salt: salt,
+        passwordHash: value
+    };
+}
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+}, function (username, password, done) {
+        let hash = sha512(password, serverSalt).passwordHash;
+        users.getUserByLoginAndPasshash(username, hash)
+            .then(user => {
+                done(user ? null : 'No user', user);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    users.getUserById(id)
+        .then(user => {
+            done(user ? null : 'No user', user);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+function checkAuth(req, res, next) {
+    if (!req.user) return res.sendStatus(401);
+    next();
+}
+
+function checkAuthor(req, res, next) {
+    events.getById(req.params.guid)
+        .then(event => {
+            if(req.params.guid !== req.user.id)
+                res.sendStatus(401);
+        })
+        .catch(err => {
+            res.sendStatus(500);
+        });
+    next();
+}
+
+function checkAdmin(req, res, next) {
+    if (req.user.role !== 'admin')
+        res.sendStatus(401);
+    next();
+}
+
+app.get('/',
+    (req,res) => res.render('index', {user: req.user}));
+
+app.get('/sign_up',
+    (req, res) => {
+        res.render('signup', {user: req.user});
+    });
+
+app.post('/sign_up',
+    (req, res) => {
+        let user = {
+            username: req.body.username,
+            password: sha512(req.body.password, serverSalt).passwordHash,
+            role: 'user'
+        };
+        users.create(user)
+            .then (() => {
+                res.redirect('/');
+            })
+            .catch(err => {
+                console.log('could not create user:', err);
+                res.redirect('/sign_up');
+            });
+    });
+
+app.get('/login', (req, res) =>  {
+    res.render('login', {user: req.user});
+});
+
+
+app.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
+app.get('/logout',
+    checkAuth,
+    (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+app.get('/events',
+    checkAuth,
+    (req,res) => {
+        events.getAll(req.user.id)
+            .then(data => {
+                event_list = data;
+                res.render('events', {event_list, user: req.user});
+            })
+            .catch(data => {
+                console.log("An error occured: ", data);
+                res.sendStatus(500);
+            });
+    });
+
+app.get('/add',
+    checkAuth,
+    (req, res) => res.render('add', {user: req.user}));
+
+app.get('/event/:guid([0-9a-f-]{24})',
+    checkAuth,
+    checkAuthor,
+    (req, res) => {
+        let event_id = req.params.guid;
+        events.getById(event_id)
+            .then(event => {
+                res.render('event', {event, user: req.user});
+            })
+            .catch(
+                err => {
+                    console.log(err);
+                    res.sendStatus(404);
+                });
+    });
+
+app.get('/search',
+    checkAuth,
+    (req, res) => {
+    let found = [];
+    if (!req.query)
+        res.render('search', {found});
+    events.getAll()
+        .then(data => {
+            event_list = data;
+            for (let i of event_list) {
+                if (i.name.indexOf(req.query.input) !== -1)
+                    found.push(i);
+            }
+            res.render('search', {found, user: req.user});
+        })
+        .catch(data => {
+            console.log("An error occurred: ", data);
+            res.sendStatus(500);
+        });
+});
+
+app.get('/everyone',
+    checkAuth,
+    checkAdmin,
+    (req, res) => {
+        let user_list = [];
+        users.getAll()
+            .then(data => {
+                user_list = data;
+                res.render('everyone', {user_list, user: req.user});
+            })
+            .catch(data => {
+                console.log("An error occured: ", data);
+                res.sendStatus(500);
+            });
+    });
+
+app.post('/add', upload.single('pic'),
+    checkAuth,
+    (req, res) => {
+    console.log(req.file);
+    events.create(construcor.Event(req.body.name, req.body.place, req.body.duration, req.body.date),
+        '/images/' + req.file.filename, req.user.id)
+        .catch(err => {console.log('An error while creating: ', err, "\n");});
+    res.redirect('/events');
+});
+
+app.post('/event/delete/:guid([0-9a-f-]{24})',
+    checkAuth,
+    checkAuthor,
+    (req, res) => {
+        console.log('trying to del', req.originalUrl);
+        let event_id = req.params.guid;
+        events.remove(event_id)
+            .then(() => {
+                res.redirect('/events');
+            })
+            .catch(
+                err => {
+                    console.log(err);
+                    res.sendStatus(404);
+                });
+    });
+
+
+app.get('/api/v1/', (req, res) => {
+    let d = "localhost:3000";
+    let resp = {
+        user_data: d + "/api/v1/user",
+        user_events: d + "api/vi/user/"
+    };
+    res.send();
+});
+
+let event_list;
+
+let portNum = config.port;
+app.listen(portNum, () => console.log(`Server started on port ${portNum}.`));
---
+++ labs/lab7/modules/events.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/lab6';
+mongoose.connect(url);
+
+let Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+let eventSch = new Schema({
+    name: String,
+    place: String,
+    duration: Number,
+    date: String,
+    picture: String,
+    authorId: String
+});
+
+eventSch.options.toJSON = {
+    transform: function(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+};
+
+let Event = mongoose.model('Event', eventSch, 'events');
+
+function create(x, pic, aId) {
+    return new Promise(function (resolve, reject) {
+        let newEvent = new Event ({
+            name: x.name,
+            place: x.place,
+            duration: x.duration,
+            date: x.date,
+            picture: pic,
+            authorId: aId
+        });
+
+        newEvent.save()
+            .then(() => {
+                resolve("successfuly created new event");
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+}
+
+function getAll(authorId) {
+    return new Promise(function (resolve, reject) {
+        Event.find({authorId: authorId}, function (err, docs) {
+            if (err)
+                reject(err);
+            resolve(JSON.parse(JSON.stringify(docs)));
+        });
+    });
+}
+
+function getById(x_id) {
+    return new Promise(function (resolve, reject) {
+        Event.findById(x_id, function (err, event){
+            if (err)
+                reject(err);
+            if (!event)
+                reject('no event wiht such id');
+            resolve(JSON.parse(JSON.stringify(event)));
+        } );
+    });
+}
+
+function remove(x_id) {
+    return new Promise(function (resolve, reject) {
+        Event.findById(x_id, function (err, event) {
+            if (err)
+                reject(err);
+            if (!event)
+                reject('no event wiht such id');
+            event.remove();
+            resolve("event deleted");
+        } );
+    });
+}
+
+
+module.exports.create = create;
+module.exports.getAll = getAll;
+module.exports.getById = getById;
+module.exports.remove = remove;
